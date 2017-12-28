348efddf9785dad4e2f6f7eac4e29de3865ae491
DESKTOP-B9FI40S\zxp <zxp@sxun.cn>
2017-12-28 13:36:00 +0800

    init 初始化

---
+++ src/main/java/com/zxp/java/utils/HttpUtil.java
@@ -0,0 +1,8 @@
+package com.zxp.java.utils;
+
+/**
+ * Created by zxp on 2017/12/26.
+ * desc:
+ */
+public class HttpUtil {
+}
---
+++ src/main/java/com/zxp/java/utils/ListUtil.java
@@ -0,0 +1,83 @@
+package com.zxp.java.utils;
+
+import java.util.*;
+
+/**
+ * Created by zxp on 2017/12/26.
+ * desc:
+ */
+public class ListUtil {
+
+    /**
+     *  获取两个集合的不同元素
+     * @param collmax
+     * @param collmin
+     * @return
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static Collection getDiffent(Collection collmax,Collection collmin)
+    {
+        //使用LinkeList防止差异过大时,元素拷贝
+        Collection csReturn = new LinkedList();
+        Collection max = collmax;
+        Collection min = collmin;
+        //先比较大小,这样会减少后续map的if判断次数
+        if(collmax.size()<collmin.size())
+        {
+            max = collmin;
+            min = collmax;
+        }
+        //直接指定大小,防止再散列
+        Map<Object,Integer> map = new HashMap<Object,Integer>(max.size());
+        for (Object object : max) {
+            map.put(object, 1);
+        }
+        for (Object object : min) {
+            if(map.get(object)==null)
+            {
+                csReturn.add(object);
+            }else{
+                map.put(object, 2);
+            }
+        }
+        for (Map.Entry<Object, Integer> entry : map.entrySet()) {
+            if(entry.getValue()==1)
+            {
+                csReturn.add(entry.getKey());
+            }
+        }
+        return csReturn;
+    }
+
+    public static Collection getDiffent2(Collection collmax,Collection collmin){
+        //使用LinkeList防止差异过大时,元素拷贝
+        Collection csReturn = new LinkedList();
+        Collection max = collmax;
+        Collection min = collmin;
+
+        //直接指定大小,防止再散列
+        Map<Object,Integer> map = new HashMap<Object,Integer>(min.size());
+        for (Object object : min) {
+            map.put(object, 1);
+        }
+        for(Object object : max){
+            if(map.get(object)==null)
+            {
+                csReturn.add(object);
+            }
+        }
+
+        return csReturn;
+    }
+    /**
+     *  获取两个集合的不同元素,去除重复
+     * @param collmax
+     * @param collmin
+     * @return
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static Collection getDiffentNoDuplicate (Collection collmax,Collection collmin)
+    {
+        return new HashSet(getDiffent(collmax, collmin));
+    }
+}
